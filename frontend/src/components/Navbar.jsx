430cfe25c1de95ba3fa41c25e7eb00785166c0e9
pawan yadav <278889633+pawan244701@users.noreply.github.com>
2026-07-15 16:00:56 +0530

    adding hambuger to nnavbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react'
 import './Navbar.css'
+
 export const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
     return (
         <div className="main-container">
             <div className="logo-container">
                 <a href="#" >Logo</a>
             </div>
-            <div className='nav-links-container'>
+            <div className={`nav-links-container ${isOpen ? 'open' : ''}`}>
                 <a href="#" className='nav-links'>Home</a>
                 <a href="#" className='nav-links'>Games</a>
                 <a href="#" className='nav-links'>Anime</a>
@@ -14,6 +21,7 @@ export const Navbar = () => {
                 <button>Login</button>
                 <button>Signup</button>
             </div>
+            <button className='hamburger-menu' onClick={ toggleMenu }>&#9776;</button>
         </div>
-    )
-}
+    );
+};
