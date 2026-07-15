9e1ab84b9d31d9cb5ad5c417696b17635871cf70
pawan yadav <278889633+pawan244701@users.noreply.github.com>
2026-07-15 17:36:18 +0530

    fixed some layout and tried to make it responsive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,17 +11,19 @@ export const Navbar = () => {
         <div className="main-container">
             <div className="logo-container">
                 <a href="#" >Logo</a>
+            </div >
+            <div className={`toggle-container ${isOpen ? 'open' : ''}`}>
+                <div className='nav-links-container'>
+                    <a href="#" className='nav-links'>Home</a>
+                    <a href="#" className='nav-links'>Games</a>
+                    <a href="#" className='nav-links'>Anime</a>
+                </div>
+                <div className='nav-button-container'>
+                    <button>Login</button>
+                    <button>Signup</button>
+                </div>
             </div>
-            <div className={`nav-links-container ${isOpen ? 'open' : ''}`}>
-                <a href="#" className='nav-links'>Home</a>
-                <a href="#" className='nav-links'>Games</a>
-                <a href="#" className='nav-links'>Anime</a>
-            </div>
-            <div className='nav-button-container'>
-                <button>Login</button>
-                <button>Signup</button>
-            </div>
-            <button className='hamburger-menu' onClick={ toggleMenu }>&#9776;</button>
+            <button className='hamburger-menu' onClick={toggleMenu}>&#9776;</button>
         </div>
     );
 };
