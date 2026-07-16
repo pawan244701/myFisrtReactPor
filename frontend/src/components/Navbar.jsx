dae0a32d96786b09e78923dc8146a0eb20302e8f
pawan yadav <278889633+pawan244701@users.noreply.github.com>
2026-07-16 17:42:06 +0530

    signup, navbar css and jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import './Navbar.css'
+import { useState } from 'react';
+import styles from './Navbar.module.css';
 
 export const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -8,22 +8,24 @@ export const Navbar = () => {
         setIsOpen(!isOpen);
     };
     return (
-        <div className="main-container">
-            <div className="logo-container">
-                <a href="#" >Logo</a>
+        <div className={styles["main-container"]}>
+            <div className={styles["logo-container"]}>
+                <a href="#" className={styles['logo-links']}>Logo</a>
             </div >
-            <div className={`toggle-container ${isOpen ? 'open' : ''}`}>
-                <div className='nav-links-container'>
-                    <a href="#" className='nav-links'>Home</a>
-                    <a href="#" className='nav-links'>Games</a>
-                    <a href="#" className='nav-links'>Anime</a>
+
+            <div className={`${styles['toggle-container']} ${isOpen ? styles.open : ''}`}>
+                <div className={styles['nav-links-container']}>
+                    <a href="#" className={styles['nav-links']}>Home</a>
+                    <a href="#" className={styles['nav-links']}>Games</a>
+                    <a href="#" className={styles['nav-links']}>Anime</a>
                 </div>
-                <div className='nav-button-container'>
-                    <button>Login</button>
-                    <button>Signup</button>
+            
+                <div className={styles['nav-button-container']}>
+                    <button className={styles['nav-buttons']}>Login</button>
+                    <button className={styles['nav-buttons']}>Signup</button>
                 </div>
             </div>
-            <button className='hamburger-menu' onClick={toggleMenu}>&#9776;</button>
+            <button className={styles['hamburger-menu']} onClick={toggleMenu}>&#9776;</button>
         </div>
     );
 };
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,14 +1,29 @@
+import styles from './Signup.module.css';
 export const Signup = () => {
     return (
-        <div>
-            <form>
-                <h1>Signup</h1>
-                <div>
-                    <label htmlFor="name">Enter Name</label>
-                    <input type="text" id="name" placeholder="my name" autoComplete="name" required />
-                </div>
-                <label htmlFor=""></label>
-            </form>
-        </div>
+        <main className={styles['signup-body']}>
+            <div className={styles['signup-container']}>
+                <h1 className={styles['signup-h1']}>Signup</h1>
+                <form className={styles['signup-form']}>
+                    <div className={styles['input-group']}>
+                        <label className={styles['signup-label']} htmlFor="username">Enter Username</label>
+                        <input className={styles['signup-input']} type="text" id="username" placeholder="my name" autoComplete="username" required />
+                    </div>
+                    <div className={styles['input-group']}>
+                        <label className={styles['signup-label']} htmlFor="email">Enter Email</label>
+                        <input className={styles['signup-input']} type="email" id="email" placeholder="example@email.com" autoComplete="email" required />
+                    </div>
+                    <div className={styles['input-group']}>
+                        <label className={styles['signup-label']} htmlFor="password">Enter Password</label>
+                        <input className={styles['signup-input']} type="password" id="password" placeholder="my$#%46pdhF76" autoComplete="new-password" required />
+                    </div>
+                    <div className={styles['input-group']}>
+                        <label className={styles['signup-label']} htmlFor="confirmPassword">Re-enter Password</label>
+                        <input className={styles['signup-input']} type="password" id="confirmPassword" placeholder="my$#%46pdhF76" autoComplete="current-password" required />
+                    </div>
+                    <button className={styles['signup-button']} type="submit">Submit</button>
+                </form>
+            </div>
+        </main>
     )
 }
