090e207cebd14f059943e8a46216420d75af7f6d
pawan yadav <278889633+pawan244701@users.noreply.github.com>
2026-07-22 16:00:07 +0530

    commiting app.* index.css Home.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,23 +16,26 @@ import { Footer } from './components/Footer';
 
 function App() {
   return (
-    <div>
+    <div className='app-container'>
       <Navbar />
       {/* placed here so that it can be renderd on all pages. */}
-      <Routes>
-        {/* Routes : thsi will be holding all of our route */}
 
-        {/* Route : these can be multiples in a return, these are indivisual... like: i can say URL, It works like: path = what is in URL e.g. : my-site.com/ and element = is what React will render on the associated path */}
-        <Route path='/' element={<Home />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/privacy' element={<Privacy />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/games' element={<Games />} />
-        {/* NOte : here should not be any tralling spaces like: path='/games ' it should be path='/games' othetwise it won't work */}
-      </Routes>
+      <main className='main-container'>
+        <Routes>
+          {/* Routes : thsi will be holding all of our route */}
+
+          {/* Route : these can be multiples in a return, these are indivisual... like: i can say URL, It works like: path = what is in URL e.g. : my-site.com/ and element = is what React will render on the associated path */}
+          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/privacy' element={<Privacy />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/games' element={<Games />} />
+          {/* NOte : here should not be any tralling spaces like: path='/games ' it should be path='/games' othetwise it won't work */}
+        </Routes>
+      </main>
       <Footer />
     </div>
   )
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,30 +2,14 @@ import { useState } from "react";
 import { Navbar } from '../components/Navbar'
 import { Login } from "./Login";
 import { Signup } from './Signup';
+import { Hero } from "../components/Hero";
 
 import './Home.module.css';
 
 export const Home = () => {
     return (
-        <div>
-            <h1>this is hoem page</h1>
-            <h1>this is hoem page</h1>
-            <h1>this is hoem page</h1>
-            <h1>this is hoem page</h1>
-            <h1>this is hoem page</h1>
-            <h1>this is hoem page</h1>
-            <h1>this is hoem page</h1>
-            <h1>this is hoem page</h1>
-            <h1>this is hoem page</h1>
-            <h1>this is hoem page</h1>
-            <h1>this is hoem page</h1>
-            <h1>this is hoem page</h1>
-            <h1>this is hoem page</h1>
-            <h1>this is hoem page</h1>
-            <h1>this is hoem page</h1>
-            <h1>this is hoem page</h1>
-            <h1>this is hoem page</h1>
-            <h1>this is hoem page</h1>
-        </div>
+        <main>
+            <Hero />
+        </main>
     )
 };
