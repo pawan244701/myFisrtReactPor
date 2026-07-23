eb463bbf04340d944b3be025ad603c5480855ee6
pawan yadav <278889633+pawan244701@users.noreply.github.com>
2026-07-23 18:56:51 +0530

    uncompleted techStack.*

---
+++ frontend/src/components/TechStack.jsx
@@ -0,0 +1,8 @@
+import styles from './TechStack.module.css'
+export const TechStack = () => {
+    return (
+        <section className={styles['main-section']}>
+            <h1>mem</h1>
+        </section>
+    )
+}
