3ea3b72cdb3a581362f33bdb9f49d0265e423168
pawan yadav <278889633+pawan244701@users.noreply.github.com>
2026-07-23 18:46:22 +0530

    commiting Hero component

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -6,6 +6,12 @@ export const Hero = () => {
             <header className={styles['main-header']}>
                 <h1>Hi, I'm Pawan Yadav</h1>
                 <h2>Welcome to the project</h2>
+                <p>I'm open to get Good Advices so if you have any message me on
+                    <a href="https://linkedin.com/in/pawan244701/"
+                        target="_blank"
+                        rel='noopener noreferrer'
+                        className={styles['linkedin']}
+                    > Linkedin</a></p>
                 <p><strong>A Full-stack learner and Frontend developer</strong></p>
                 <p>This project is in existence because I want to learn React.js and build something using React</p>
             </header>
