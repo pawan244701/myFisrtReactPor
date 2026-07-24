24642ed8686d59456f37120444a1638f09074f8c
pawan yadav <278889633+pawan244701@users.noreply.github.com>
2026-07-24 12:01:01 +0530

    commitign TechStack *

--- frontend/src/components/TechStack.jsx
+++ frontend/src/components/TechStack.jsx
@@ -2,7 +2,37 @@ import styles from './TechStack.module.css'
 export const TechStack = () => {
     return (
         <section className={styles['main-section']}>
-            <h1>mem</h1>
+            <h1>My Tech Stack</h1>
+            <div className={styles['secondery-container']}>
+                <article className={styles['stack-card']}>
+                    <h3>Design: </h3>
+                    <ul>
+                        <li>Figma</li>
+                    </ul>
+                </article>
+                <article className={styles['stack-card']}>
+                    <h3>Frontend: </h3>
+                    <ul>
+                        <li>HTML5</li>
+                        <li>CSS3</li>
+                        <li>JavaScript (ES-6+)</li>
+                        <li>React.js</li>
+                    </ul>
+                </article>
+                <article className={styles['stack-card']}>
+                    <h3>Backend: </h3>
+                    <ul>
+                        <li>Node.js</li>
+                        <li>Express.js</li>
+                    </ul>
+                </article>
+                <article className={styles['stack-card']}>
+                    <h3>Database: </h3>
+                    <ul>
+                        <li>MySQL</li>
+                    </ul>
+                </article>
+            </div>
         </section>
     )
 }
