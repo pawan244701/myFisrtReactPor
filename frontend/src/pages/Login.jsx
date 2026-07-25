c3d20e86830691862404b66e1cc0b3a38adc3524
pawan yadav <278889633+pawan244701@users.noreply.github.com>
2026-07-25 18:55:51 +0530

    ...

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,18 +1,105 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // i imported it like: import { Navigate } from 'react-router-dom'; adn fucked my mind ~ for 1/2 hour
 import styles from './Login.module.css'
+
 export const Login = () => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const validateForm = () => {
+        const isPasswordLenghtOk = password.length >= 8;
+        return isPasswordLenghtOk;
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const isValid = validateForm();
+        if (!isValid) {
+            alert('Password must be at least 8 characters long.');
+            return;
+        }
+
+        try {
+            const response = await fetch('endPoint/url', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username,
+                    password
+                }),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                sessionStorage.setItem('authUser', username);
+                navigate(`/`);
+            }
+            else {
+                // alert('login failed. because NO DB and server is backing it.');
+                sessionStorage.setItem('authUser', username);
+                navigate(`/`);
+
+            }
+        } catch (error) {
+            // console.error('loging error :', error);
+            sessionStorage.setItem('authUser', username);
+            // alert('because NO Server is backing it for now. it is pure frontend');
+        }
+    };
+
     return (
         <main className={styles['login-container']}>
-            <form className={styles['login-form']}>
+            <form
+                onSubmit={handleSubmit}
+                className={styles['login-form']}>
                 <h1 className={styles['login-h1']}>Login</h1>
+
                 <div className={styles['input-group']}>
-                    <label className={styles['login-label']} htmlFor="username">Username</label>
-                    <input className={styles['login-input']} type="text" id="username" placeholder="username" autoComplete="username" required />
+                    <label
+                        className={styles['login-label']}
+                        htmlFor="username"
+                    >Username
+                    </label>
+                    <input
+                        className={styles['login-input']}
+                        type="text"
+                        id="username"
+                        name='username'
+                        placeholder="username"
+                        autoComplete="username"
+                        required
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
                 </div>
                 <div className={styles['input-group']}>
-                    <label className={styles['login-label']} htmlFor="password">Password</label>
-                    <input className={styles['login-input']} type="password" id="password" placeholder="my$#%46pdhF76" autoComplete="current-password" required />
+                    <label
+                        className={styles['login-label']}
+                        htmlFor="password"
+                    >Password
+                    </label>
+                    <input
+                        className={styles['login-input']}
+                        type="password"
+                        id="password"
+                        name='password'
+                        placeholder="my$#%46pdhF76"
+                        autoComplete="current-password"
+                        required
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
-                <button className={styles['login-button']} type="submit">Login</button>
+                <button
+                    className={styles['login-button']}
+                    type="submit"
+                >Login
+                </button>
             </form>
         </main>
     )
