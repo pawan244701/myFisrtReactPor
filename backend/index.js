c4a9f8e4b480b94e18f05d017b3dac5eea17ceca
pawan yadav <278889633+pawan244701@users.noreply.github.com>
2026-07-30 12:56:10 +0530

    addign /verify-otp route

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,8 @@ import { json } from 'express';
 import cors from 'cors';
 import { pool } from './config/database.js';
 import { router } from './routes/allRoute.js';
-import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
+import { corsFuncAllowedOrigins } from './middlewares/corsMiddle.js'
 
 const app = express();
 // helmet: a surity middleware that works by autom... configring and settign HTTP res header on the server
@@ -13,37 +13,11 @@ const app = express();
 app.use(helmet());
 app.use(express.json());
 
-const allowedOrigins = [process.env.MY_SITE_FRONTEND_URL];
+app.use(cors());
+app.use(corsFuncAllowedOrigins);
 
-// cors setuop to only my mysite
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-};
-app.use(cors(corsOptions));
-
-// rateLimit an exteranl package that helps us to track incomign reqs from client
-// so that we genrally protect server to attacks like: brutefroce
-const authLimiter = rateLimit({
-    windowMs: 10 * 60 * 1000, // seting 10 minutes
-    max: 10,                  // eact IP 10 attempts
-    message: {
-        status: 429,
-        error: 'Too many Requests',
-        message: 'Too many attempts! Please try again afert 10 minuts.'
-    }
-});
-app.set('trust proxy', 1); // i'm usign render adn render works as proxy, so this line ignore the render's ip and restrict only user
-app.use('/login', authLimiter);
-app.use('/signup', authLimiter);
+// i'm usign render adn render works as proxy, so this line ignore the render's ip and restrict only user
+app.set('trust proxy', 1);
 
 app.use('/', router);
 
--- backend/routes/allRoute.js
+++ backend/routes/allRoute.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { userSignup } from '../controllers/signUpController.js'; // don't forget to add extansion .js otherwise you'll get: Error [ERR_MODULE_NOT_FOUND]: Cannot find module
+import { authLimiter } from '../middlewares/rateLimiter.js';
+
+import { tempUserSignup } from '../controllers/tempSignUpController.js'; // don't forget to add extansion .js otherwise you'll get: Error [ERR_MODULE_NOT_FOUND]: Cannot find module
+import { verifySignUpOtp } from '../controllers/verifySignUpOtpController.js';
 import { userLogin } from '../controllers/loginController.js'; // don't forget to import otherwise you'll get: Error [ReferenceError: userLogin is not defined]
 
 export const router = express.Router();
 
-router.post('/signup', userSignup);
-router.post('/login', userLogin);
-router.post('/login', userLogin);
-// router.post('/contact', userMessage);
+router.post('/signup', authLimiter, tempUserSignup);
+router.post('/verify-otp', authLimiter, verifySignUpOtp);
+router.post('/login', authLimiter, userLogin);
+// router.post('/contact',authLimiter, userMessage);
