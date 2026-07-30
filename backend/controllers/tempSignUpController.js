93cc27de73e4095902f2388d66db3dcbc0c444fc
pawan yadav <278889633+pawan244701@users.noreply.github.com>
2026-07-30 12:54:18 +0530

    installing @Brevo adn breaking signup BC file into 2 saperate files and addign email verification logic

---
+++ backend/controllers/tempSignUpController.js
@@ -0,0 +1,85 @@
+import 'dotenv/config';
+import { pool } from '../config/database.js';
+import bcrypt from 'bcrypt';
+import { BrevoClient } from '@getbrevo/brevo';
+
+export const tempUserSignup = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+        return res.status(400).json({
+            message: "Please provide all details"
+        });
+    }
+    const otpclient = new brevo.BrevoClient({
+        apiKey: process.env.BREVO_API_KEY
+    });
+    try {
+        // checking if already exists in usres tbl
+        const [rows] = await pool.query(
+            "SELECT * FROM users WHERE email = ? LIMIT 1",
+            [email]
+        );
+        if (rows.length > 0) {
+            return res.status(400).json({
+                message: 'User Exists! Try Login with password'
+            });
+        }
+
+        const [isOtpInTempTable] = await pool.query(
+            "SELECT * FROM pendingVerificationUsers WHERE email = ? AND expiresAt > NOW()",
+            [email]
+        );
+        if (isOtpInTempTable.length > 0) {
+            return res.status(400).json({
+                success: false,
+                message: "OTP is already sended, check your email or try after 5 minutes!"
+            });
+        }
+
+        // otp gen
+        const genOtp = Math.floor(1000000 + Math.random() * 9000000).toString();
+        const expireAt = new Date(Date.now() + 5 * 60 * 1000);
+
+        // password incryption
+        const saltRound = 8;
+        const hashedPassword = await bcrypt.hash(password, saltRound);
+
+        //saving data in DB table named: pendingVerificationUsers
+        await pool.query(
+            `INSERT INTO pendingVerificationUsers (username, email, password, otpCode, expiresAt)
+                 VALUES (?, ?, ?, ?, ?)
+                 ON DUPLICATE KEY UPDATE 
+                    username = VALUE(username),
+                    password = VALUE(password),
+                    otpCode = VALUE(otpCode),
+                    expiresAt = VALUE(expiresAt)`,
+            [username, email, hashedPassword, genOtp, expiresAt]
+        );
+
+        // send otp email 
+        await otpclient.transactionalEmails.sendTransacEmail({
+            subject: 'Otp to verify your Email',
+            textContent: `This is your OTP: ${genOtp}. \nIt is valid only for 5 minites.`,
+            sender: { "name": "pawan244701", "email": process.env.MY_EMAIL },
+            to: [{ "email": email }]
+        });
+
+        // checking for valid otp
+        return res.status(200).json({
+            message: 'Otp send to your email! Check your email app.'
+        });
+
+    } catch (err) {
+        if (err.code === 'ER_DUP_ENTRY' || err.errno === 1062) {
+            return res.status(400).json({
+                success: false,
+                message: 'Email already Exists! try diffrent one or login with current one.'
+            });
+        }
+        //console.error("Signup error details:", err);
+        return res.status(500).json({
+            message: 'Signup failed! Server error!'
+        });
+    }
+}
---
+++ backend/controllers/verifySignUpOtpController.js
@@ -0,0 +1,48 @@
+import 'dotenv/config';
+import { pool } from '../config/database.js';
+
+export const verifySignUpOtp = async (req, res) => {
+    const { email, otpCode } = req.body;
+
+    if (!email || !otpCode) {
+        return res.status(400).json({
+            message: "Email and Otp code is Required."
+        });
+    }
+
+    try {
+        const [rows] = await pool.query(
+            "SELECT * FROM pendingVerificationUsers WHERE email = ? AND otpCode = ?",
+            [email, otpCode]
+        );
+        if (rows.length === 0) {
+            return res.status(400).json({
+                message: "Invalid Otp or email!"
+            });
+        }
+        const pendingUser = rows[0];
+        if (new Date() > new Date(pendingUser.expiresAt)) {
+            return res.status(400).json({
+                message: "Otp expired! Please Signup again."
+            });
+        }
+
+        await pool.query(
+            "INSERT INTO users (username, email, password) VALUES (?,?,?)",
+            [pendingUser.username, pendingUser.email, pendingUser, password]
+        );
+        await pool.query(
+            "DELETE FROM pendingVerificationUsers WHERE email = ?",
+            [email]
+        );
+        return res.status(201).json({
+            message: 'Email verified successfully!'
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: "Server error. Please try again after a few minutes."
+        });
+    }
+}
