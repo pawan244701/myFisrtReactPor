76e183057010805f89486da6ed353d3a48d8b8de
pawan yadav <278889633+pawan244701@users.noreply.github.com>
2026-08-15 19:28:54 +0530

    commiting ratelimiter

---
+++ backend/middlewares/rateLimiter.ts
@@ -0,0 +1,15 @@
+
+import rateLimit from 'express-rate-limit';
+
+
+// rateLimit an exteranl package that helps us to track incomign reqs from client
+// so that we genrally protect server to attacks like: brutefroce
+export const authLimiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // seting 10 minutes
+    max: 10,                  // eact IP 10 attempts
+    message: {
+        status: 429,
+        error: 'Too many Requests',
+        message: 'Too many attempts! Please try again afert 10 minutes.'
+    }
+});
