c02992d36cc8ef1bd7d7dd0e31a76e7aee05a956
pawan yadav <278889633+pawan244701@users.noreply.github.com>
2026-08-16 13:42:52 +0530

    deleted games files

--- frontend/src/pages/Games.jsx
+++
@@ -1,8 +0,0 @@
-import styles from './Games.module.css';
-export const Games = () => {
-    return (
-        <div className={styles['jast-for-now']}>
-            <h1>NOTE: I will be re-creating games in React but i'll take a lil whiel</h1>
-        </div>
-    )
-}
