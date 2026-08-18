bd456502ceb29d0be36fdfe60a7f6bbc3b149ce4
pawan yadav <278889633+pawan244701@users.noreply.github.com>
2026-08-18 12:33:07 +0530

    ADDING MIDDLEWARE: ipLogger

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
-import { json } from 'express';
 import cors from 'cors';
 import { router } from './routes/allRoute.js';
 import helmet from 'helmet';
-import { corsFuncAllowedOrigins } from './middlewares/corsMiddle.js'
+import { corsFuncAllowedOrigins } from './middlewares/corsMiddle.js';
+import { ipLogger } from './middlewares/middleIpLogger.js';
 
 const app = express();
+app.set('trust proxy', 1);
 // helmet: a surity middleware that works by autom... configring and settign HTTP res header on the server
 // to send to client preventing: XXS, MIME, ClickJacking liek attacks always used at the top of routes
 app.use(helmet());
@@ -16,8 +17,7 @@ app.use(cors());
 app.use(corsFuncAllowedOrigins);
 
 // i'm usign render adn render works as proxy, so this line ignore the render's ip and restrict only user
-app.set('trust proxy', 1);
-
+app.use(ipLogger);
 app.use('/', router);
 
 const PORT = process.env.PORT || process.env.SERVER_PORT;
